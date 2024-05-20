545a1be510916b1fd3cac007a1e7ed27d04d49e9
juanadls <eleo.dev09@gmail.com>
2024-05-20 07:39:34 -0300

    request is 200 from server

--- lib/data/services/service_constants/service_contants.dart
+++ lib/data/services/service_constants/service_contants.dart
@@ -1,4 +1,4 @@
 abstract class ServiceContants {
   static const getEndpoint =
-      'https://api.edamam.com/api/recipes/v2?type=public&q=banana&app_id=3c0b0cac&app_key=3c0b0cac&ingr=ingr%3D3-9&diet=high-protein&mealType=Breakfast&time=time%3D1%252B&imageSize=REGULAR';
+      'https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=3c0b0cac&app_key=%201740dd117fe23d41d8d372cf4b3d3c33%09&diet=high-protein&mealType=Lunch&imageSize=REGULAR';
 }
